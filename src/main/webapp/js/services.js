e173cf21166c9f6ed3578eecf52010e760b22389
Matyas Danter <mdanter@gmail.com>
2014-06-26 16:55:47 -0400

    Changed URI

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -3,7 +3,7 @@
 angular.module('demoApp.services', [ 'ngResource' ]).factory('AvailableTasks', function($http) {
 	return {
 		get : function(params) {
-			return $http.get('/web-reference-1.1.0/services/task/get-available/admin');
+			return $http.get('/angularjs-jbpm/api/rest/task/available/abaxter');
 		}
 	};
 });
